d51cf8fa7ba3cba5979cc7838096407215fd1403
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-08-07 07:30:48 +0530

    Commit @ 2022-08-07 07:30:48

--- init.lua
+++ init.lua
@@ -147,7 +147,6 @@ return packer.startup(function(use)
       dashboard.section.header.val = {
         [[ ██╗   ██╗ ]],
         [[ ██║   ██║ ]],
-        [[ ██║   ██║ ]],
         [[ ╚██╗ ██╔╝ ]],
         [[  ╚████╔╝  ]],
         [[   ╚═══╝   ]],
