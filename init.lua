32a40c864d345f64a5b241ebbfb380f8f3b78e38
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-08-11 17:50:48 +0530

    Commit @ 2022-08-11 17:50:48

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- OPTIONS
 local g = vim.g
 local o = vim.opt
+local a = vim.api
 
 o.termguicolors = true
 -- o.background = 'dark'
@@ -69,10 +70,10 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
-  vim.cmd[[packadd packer.nvim]]
+  a.nvim_command('packadd packer.nvim')
 end
 
-vim.cmd("autocmd BufWritePost init.lua source <afile> | PackerCompile")
+a.nvim_create_autocmd("BufWritePost", { pattern = "init.lua", command = "source <afile> | PackerSync" })
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
