10b13d9fffbc3edef384c50f541976c8ac4b79d7
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-08-15 22:41:13 +0530

    Commit @ 2022-08-15 22:41:13

--- init.lua
+++ init.lua
@@ -148,18 +148,20 @@ local function cmp_setup()
           fallback()
         end
       end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function()
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
         elseif luasnip.jumpable(-1) then
           luasnip.jump(-1)
+        else
+          fallback()
         end
       end, { "i", "s" }),
     },
     sources = {
       { name = "nvim_lsp" },
       { name = "nvim_lua" },
-      { name = 'luasnip' },
+      { name = "luasnip" },
       { name = "buffer" },
       { name = "path" },
       { name = "treesitter" },
