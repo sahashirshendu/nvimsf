9267ad81da9e9530daa2e41997f7f074e692af48
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-10-06 13:23:44 +0530

    Commit @ 2022-10-06 13:23:44

--- init.lua
+++ init.lua
@@ -90,8 +90,9 @@ local function snip_setup()
 
   -- Load Snippets
   require("luasnip.loaders.from_vscode").lazy_load()
-  require("luasnip.loaders.from_snipmate").lazy_load({ paths = snip_folder })
-  require("luasnip.loaders.from_lua").lazy_load({ paths = snip_folder })
+  -- require("luasnip.loaders.from_vscode").lazy_load({ paths = snip_folder })
+  -- require("luasnip.loaders.from_snipmate").lazy_load({ paths = snip_folder })
+  -- require("luasnip.loaders.from_lua").lazy_load({ paths = snip_folder })
 end
 
 -- CMP
@@ -193,7 +194,7 @@ local function lsp_setup()
   local formatting = null_ls.builtins.formatting
   local diagnostics = null_ls.builtins.diagnostics
 
-  api.nvim_create_user_command("Format", "lua vim.lsp.buf.formatting()", {}) -- Builtin Formatting of NVim LSP
+  api.nvim_create_user_command("Format", "lua vim.lsp.buf.format({ async = true })", {}) -- Builtin Formatting of NVim LSP
 
   null_ls.setup({
     debug = false,
@@ -296,7 +297,7 @@ local function lsp_setup()
     if client.server_capabilities.documentFormattingProvider then
       local function format()
         local view = vim.fn.winsaveview()
-        vim.lsp.buf.formatting({
+        vim.lsp.buf.format({
           async = true,
           filter = function(attached_client)
             return attached_client.name ~= ""
@@ -362,7 +363,7 @@ local function lsp_setup()
     end
     -- Disable Formatting for tsserver, sumneko-lua
     if client.name == "tsserver" or client.name == "sumneko_lua" then
-      client.resolved_capabilities.document_formatting = false
+      client.server_capabilities.document_formatting = false
     end
 
     -- require("keymaps").setup(client, bufnr)
