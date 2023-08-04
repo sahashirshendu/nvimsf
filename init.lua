11e8dd826b06081ac46230144d60dfdc09c563ca
Shirshendu Saha <shirshendusaha78@gmail.com>
2023-08-04 11:43:41 +0530

    Commit @ 2023-08-04 11:43:41

--- init.lua
+++ init.lua
@@ -76,7 +76,7 @@ local function lsp_setup()
       settings = {
         Lua = {
           runtime = {version = 'LuaJIT'},
-          workspace = {library = vim.api.nvim_get_runtime_file('', true), checkThirdParty = false},
+          workspace = {library = api.nvim_get_runtime_file('', true), checkThirdParty = false},
           telemetry = {enable = false},
         },
       },
@@ -318,6 +318,6 @@ local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath})
 end
-vim.opt.rtp:prepend(lazypath)
+o.rtp:prepend(lazypath)
 
 require('lazy').setup(plugins, {ui = {border = 'single'}})
