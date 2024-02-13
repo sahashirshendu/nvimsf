0a93845441adc0c4dd472d807daed566087daf69
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2024-02-13 21:29:09 +0530

    Commit @ 2024-02-13 21:29:09

--- init.lua
+++ init.lua
@@ -204,36 +204,17 @@ local plugins = {
     },
     config = function()
       local cmp = require('cmp')
-      local luasnip = require('luasnip')
-      local has_words_before = function()
-        local line, col = unpack(api.nvim_win_get_cursor(0))
-        return col ~= 0 and api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-      end
       cmp.setup({
         completion = {completeopt = 'menu,menuone,noselect,noinsert'},
         snippet = {
           expand = function(args)
-            luasnip.lsp_expand(args.body)
+            require('luasnip').lsp_expand(args.body)
           end,
         },
         mapping = cmp.mapping.preset.insert {
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm {select = false},
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
-            elseif has_words_before() then cmp.complete()
-            else fallback()
-            end
-          end, {'i', 's'}),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then luasnip.jump(-1)
-            else fallback()
-            end
-          end, {'i', 's'}),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}),
         },
         sources = {
           {name = 'nvim_lsp'},
