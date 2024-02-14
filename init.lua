a4ee51acfe87283915a2e5ffbe2e29def559f927
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2024-02-14 10:14:11 +0530

    Commit @ 2024-02-14 10:14:11

--- init.lua
+++ init.lua
@@ -188,10 +188,8 @@ local plugins = {
 
           luasnip.config.set_config({
             history = true,
-            ext_base_prio = 200,
-            ext_prio_increase = 1,
             enable_autosnippets = true,
-            updateevents = 'TextChanged,TextChangedI',
+            update_events = "TextChanged,TextChangedI",
             store_selection_keys = '<C-q>',
           })
 
