1dc08b5baeddf9dadfc9e74d1b807e931725d3c5
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2024-10-31 21:37:03 +0530

    Commit @ 2024-10-31 21:37:03

--- init.lua
+++ init.lua
@@ -214,14 +214,8 @@ local plugins = {
           {name = 'path'},
         },
       })
-      cmp.setup.cmdline('/', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {{name = 'buffer'}},
-      })
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources{{name = 'path'}, {name = 'cmdline'}},
-      })
+      cmp.setup.cmdline('/', {mapping = cmp.mapping.preset.cmdline(), sources = {{name = 'buffer'}}})
+      cmp.setup.cmdline(':', {mapping = cmp.mapping.preset.cmdline(), sources = cmp.config.sources{{name = 'path'}, {name = 'cmdline'}}})
     end
   },
   -- LSP
