3ad4aff0c25daebd0b60d40e19bcd7e085e46b76
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2025-06-29 20:10:45 +0530

    Commit @ 2025-06-29 20:10:45

--- init.lua
+++ init.lua
@@ -149,14 +149,10 @@ local plugins = {
   },
   -- CMP
   {
-    'hrsh7th/nvim-cmp', event = 'InsertEnter',
+    'saghen/blink.cmp', event = 'InsertEnter',
     dependencies = {
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
       {
         'L3MON4D3/LuaSnip',
-        dependencies = {'saadparwaiz1/cmp_luasnip'},
         build = 'make install_jsregexp',
         config = function()
           local snip_folder = vim.fn.stdpath('config') .. '/snippets/'
@@ -170,32 +166,22 @@ local plugins = {
         end
       },
     },
-    config = function()
-      local cmp = require('cmp')
-      cmp.setup {
-        completion = {completeopt = 'menu,menuone,noselect,noinsert'},
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
+    version = '1.*',
+    opts = {
+      keymap = {
+        preset = 'enter',
+        ['<Tab>'] = {'select_next', 'snippet_forward', 'fallback'},
+        ['<S-Tab>'] = {'select_prev', 'snippet_backward', 'fallback'},
+      },
+      completion = {
+        accept = {auto_brackets = {enabled = true}},
+        documentation = {auto_show = true},
+        menu = {draw = {columns = {{'label', 'label_description', 'source_name', gap = 1}}},
         },
-        mapping = cmp.mapping.preset.insert {
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}),
-        },
-        sources = {
-          {name = 'nvim_lsp'},
-          {name = 'luasnip'},
-          {name = 'buffer'},
-          {name = 'path'},
-        },
-      }
-      cmp.setup.cmdline('/', {mapping = cmp.mapping.preset.cmdline(), sources = {{name = 'buffer'}}})
-      cmp.setup.cmdline(':', {mapping = cmp.mapping.preset.cmdline(), sources = cmp.config.sources{{name = 'path'}, {name = 'cmdline'}}})
-    end
+      },
+      signature = {enabled = true},
+      snippets = {preset = 'luasnip'},
+    },
   },
   -- LSP
   {
